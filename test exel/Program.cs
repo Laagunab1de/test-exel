b775c053e5391dd71875bf163839a442f63bdb20
andre <andreynidrih>
2023-08-03 04:34:40 +1000

    чисто на верочку добавил

--- test exel/Program.cs
+++ test exel/Program.cs
@@ -3,57 +3,67 @@ using System;
 using System.IO;
 using System.Text;
 
+
+string path2 = @"C:\Users\andre\OneDrive\Документы\kk\cross table.txt";//путь до файла 
+string path3 = @"C:\Users\andre\OneDrive\Документы\kk\phs\";//путь до файла 
+//int[] ints = new int[12] {1,2,3,4,5,6,7,8,9,10,11,12};
+//foreach (var item in ints)//форыч читает каждую строку в массиве и создает поле с этой строкой
+//{
+//    //string[] ss = item.Split(",");//разделяем строку на массив со словами
+//    byte[] photo = File.ReadAllBytes(path3 + item.ToString() + ".jpeg");
+//    //_context.Users.Add(photoss);//нужно было бы добавитиь в бд 
+//    Console.WriteLine(path3 + item.ToString() + ".jpeg");//вместо строки выше чекаем че там в юзере
+//}
 List<User> users = new List<User>();
 List<Photos> phs = new List<Photos>();
 string path = @"C:\Users\andre\OneDrive\Документы\kk\cross table1.txt";//путь до файла 
 using (FileStream fs = File.OpenRead(path))//открываем файлстрим с нашим файлом 
 {
-    string[] tt = File.ReadAllLines(path);//тута крута все читается в массив
-    foreach (var item in tt)//форыч читает каждую строку в массиве и создает поле с этой строкой
-    {
-        string[] ss = item.Split(",");//разделяем строку на массив со словами
-        User user = new User() {//каждое слово записываем в переменную 
-            name = ss[0],//чета
-            surname = ss[1],//чета тоже
-           // login = ss[2]//еще чета
-        };
-        //_context.Users.Add(user);//нужно было бы добавитиь в бд 
-        Console.WriteLine(user.name + " " + user.surname);//вместо строки выше чекаем че там в юзере
-    }
-    fs.Close();// закрываем стрим
+string[] tt = File.ReadAllLines(path);//тута крута все читается в массив
+foreach (var item in tt)//форыч читает каждую строку в массиве и создает поле с этой строкой
+{
+string[] ss = item.Split(",");//разделяем строку на массив со словами
+User user = new User()
+{//каждое слово записываем в переменную 
+name = ss[0],//чета
+surname = ss[1],//чета тоже
+                // login = ss[2]//еще чета
+};
+//_context.Users.Add(user);//нужно было бы добавитиь в бд 
+Console.WriteLine(user.name + " " + user.surname);//вместо строки выше чекаем че там в юзере
 }
-string path2 = @"C:\Users\andre\OneDrive\Документы\kk\cross table.txt";//путь до файла 
-string path3 = @"C:\Users\andre\OneDrive\Документы\kk\phs\";//путь до файла 
+fs.Close();// закрываем стрим
+}
 using (FileStream fs = File.OpenRead(path2))//открываем файлстрим с нашим файлом 
 {
-    string[] tt = File.ReadAllLines(path);//тута крута все читается в массив
-    foreach (var item in tt)//форыч читает каждую строку в массиве и создает поле с этой строкой
-    {
-        string[] ss = item.Split(",");//разделяем строку на массив со словами
-        byte[] photo = File.ReadAllBytes(path3 + ss[0]);
-        Photos photoss = new Photos()
-        {
-            Photo = photo
-        };
-        //_context.Users.Add(photoss);//нужно было бы добавитиь в бд 
-        Console.WriteLine(photoss.Photo);//вместо строки выше чекаем че там в юзере
-    }
-    //users = _context.Users.Get();
-    //phs = _context.Users.Get();
-    foreach (var item in tt)//форыч читает каждую строку в массиве и создает поле с этой строкой
-    {
-        string[] ss = item.Split(",");//разделяем строку на массив со словами
-        User i = users.Find(s => s.Id.ToString() == ss[1]);
-        CrossPhotos crossPhotos = new CrossPhotos()
-        {
-            phId = phs.Find(s => s.Id.ToString() == ss[1]).Id,
-            userid = i.Id
-        };
-        Console.WriteLine(crossPhotos.phId + " " + crossPhotos.userid);
-        //_context.Crossphotos.Add(crossPhotos);
-    }
-    fs.Close();// закрываем стрим
-    
+string[] tt = File.ReadAllLines(path);//тута крута все читается в массив
+foreach (var item in tt)//форыч читает каждую строку в массиве и создает поле с этой строкой
+{
+string[] ss = item.Split(",");//разделяем строку на массив со словами
+byte[] photo = File.ReadAllBytes(path3 + ss[0]);
+Photos photoss = new Photos()
+{
+Photo = photo
+};
+//_context.Users.Add(photoss);//нужно было бы добавитиь в бд 
+Console.WriteLine(photoss.Photo);//вместо строки выше чекаем че там в юзере
+}
+//users = _context.Users.Get();
+//phs = _context.Users.Get();
+foreach (var item in tt)//форыч читает каждую строку в массиве и создает поле с этой строкой
+{
+string[] ss = item.Split(",");//разделяем строку на массив со словами
+User i = users.Find(s => s.Id.ToString() == ss[1]);
+CrossPhotos crossPhotos = new CrossPhotos()
+{
+phId = phs.Find(s => s.Id.ToString() == ss[1]).Id,
+userid = i.Id
+};
+Console.WriteLine(crossPhotos.phId + " " + crossPhotos.userid);
+//_context.Crossphotos.Add(crossPhotos);
+}
+fs.Close();// закрываем стрим
+
 }
 
 public partial class User//моделька юзера чтобы данные внести:)
